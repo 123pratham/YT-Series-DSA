81ffe8d064b6269bb25e69c8bc9131ffa74e9127
Pratham Sahu <88877454+123pratham@users.noreply.github.com>
2024-09-04 00:30:32 +0530

    adding codes

--- lec_2.cpp
+++ lec_2.cpp
@@ -1,2 +1,14 @@
 /* Lecture 2 : Variable, Data Types & Operators | DSA Series | C++*/
 
+#include<iostream>
+using namespace std;
+// int main(){
+//     std::cout << "/* message */" << std::endl;
+// }
+int main(){
+    cout<<"Hello mere bhai"<<endl;
+    char grade = 'a';//97
+    int value = grade;
+    cout<<value<<endl;
+    return 0;
+}
