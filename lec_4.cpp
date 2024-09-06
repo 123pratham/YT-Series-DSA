ac410bdc357775c397b2ad0dcb7c9da695c40055
Pratham Sahu <88877454+123pratham@users.noreply.github.com>
2024-09-06 15:02:46 +0530

    star pattern add

--- lec_4.cpp
+++ lec_4.cpp
@@ -13,5 +13,15 @@ int main(int argc, const char** argv) {
     //     }
     // }
 
+    // int n = 4;
+    // for(int i = 1;i<=n;i++){
+    //     cout<<endl;
+    //     for (int j = 1; j <= n; j++)
+    //     {
+    //         /* code */
+    //         cout<<"*";
+    //     }
+    // }
+
     return 0;
 }
