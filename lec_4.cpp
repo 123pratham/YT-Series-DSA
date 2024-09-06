f3d3df043334485aa289eb60890ef4184fb83ea9
Pratham Sahu <88877454+123pratham@users.noreply.github.com>
2024-09-06 15:00:26 +0530

    Pattern 1

--- lec_4.cpp
+++ lec_4.cpp
@@ -3,5 +3,15 @@ using namespace std;
 int main(int argc, const char** argv) {
     //Patterns
 
+    // int n = 4;
+    // for(int i = 1;i<=n;i++){
+    //     cout<<endl;
+    //     for (int j = 1; j <= n; j++)
+    //     {
+    //         /* code */
+    //         cout<<j;
+    //     }
+    // }
+
     return 0;
 }
